62e4386cc772b43ff36e1c5aa30b646e2ac10674
Jackson <jhhemme43@gmail.com>
2021-10-06 11:22:11 -0600

    finish app about me

--- about me/ViewController.swift
+++ about me/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var imageOne: UIImageView!
+    
+    @IBOutlet weak var imageTwo: UIImageView!
+    
+    @IBOutlet weak var imageThree: UIImageView!
+    
+    @IBOutlet weak var imageFour: UIImageView!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imageOne.isHidden = true
+        imageTwo.isHidden = true
+        imageThree.isHidden = true
+        imageFour.isHidden = true
     }
 
-
+    @IBAction func myButton(_ sender: Any)
+    {
+        imageOne.isHidden = false
+        imageTwo.isHidden = false
+        imageThree.isHidden = false
+        imageFour.isHidden = false
+    }
+    
 }
 
